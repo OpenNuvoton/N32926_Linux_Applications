f8c834030561bfe3319b40d51118d6245451035e
Chih-Chiang Chang <ccchang12@nuvoton.com>
2020-07-30 15:53:20 +0800

    Update emWin library to v6.10f.2

--- emWin/Config/GUIConf.c
+++ emWin/Config/GUIConf.c
@@ -3,13 +3,13 @@
 *        Solutions for real time microcontroller applications        *
 **********************************************************************
 *                                                                    *
-*        (c) 1996 - 2018  SEGGER Microcontroller GmbH                *
+*        (c) 1996 - 2019  SEGGER Microcontroller GmbH                *
 *                                                                    *
 *        Internet: www.segger.com    Support:  support@segger.com    *
 *                                                                    *
 **********************************************************************
 
-** emWin V5.48 - Graphical user interface for embedded applications **
+** emWin V6.10 - Graphical user interface for embedded applications **
 All  Intellectual Property rights in the Software belongs to  SEGGER.
 emWin is protected by  international copyright laws.  Knowledge of the
 source code may not be used to write a similar product. This file may
@@ -33,7 +33,7 @@ License model:            emWin License Agreement, signed February 27, 2018
 Licensed platform:        Cortex-M and ARM9 32-bit series microcontroller designed and manufactured by or for Nuvoton Technology Corporation
 ----------------------------------------------------------------------
 Support and Update Agreement (SUA)
-SUA period:               2018-03-26 - 2019-03-27
+SUA period:               2018-03-26 - 2020-03-27
 Contact to extend SUA:    sales@segger.com
 ----------------------------------------------------------------------
 File        : GUIConf.c
@@ -52,7 +52,11 @@ Purpose     : Display controller initialization
 //
 // Define the available number of bytes available for the GUI
 //
+#ifdef __GUI_NUMBYTES__
+#define GUI_NUMBYTES  __GUI_NUMBYTES__
+#else
 #define GUI_NUMBYTES  0x1000   //0x20000
+#endif
 
 /*********************************************************************
 *
